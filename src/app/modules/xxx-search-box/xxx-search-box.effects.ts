4a9be7b90081d2b9dfc27f84f41e0c3c9796bc5f
AngularExample <info@angularexample.com>
2019-11-11 17:03:10 -0500

    Search box effects runs nav after action to set search text.

--- src/app/modules/xxx-search-box/xxx-search-box.effects.ts
+++ src/app/modules/xxx-search-box/xxx-search-box.effects.ts
@@ -1,7 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, createEffect, Effect, ofType } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
+import * as fromSearchBox from '@app/modules/xxx-search-box/xxx-search-box.reducer';
+import { environment } from '@env/environment';
 
 @Injectable()
 export class XxxSearchBoxEffects {
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private router: Router) {}
+
+  setSearchText$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType('[XxxSearchBox] setSearchText'),
+        tap<fromSearchBox.State>(payload => {
+          const url = [environment.url.questions];
+          const queryParams = {
+            title: payload.searchText
+          };
+          return this.router.navigate(url, { queryParams: queryParams });
+        })
+      ),
+    { dispatch: false }
+  );
 }
