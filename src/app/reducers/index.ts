c14f4754c66a8d6dd64da2acd32f501fabc393c7
AngularExample <info@angularexample.com>
2019-11-11 11:21:04 -0500

    Router integration with NgRx Store is done. Router data is stored on nav change.

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,14 +1,16 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
 
-import * as fromSearchBox from '@app/modules/xxx-search-box/xxx-search-box.reducer';
 import { environment } from '@env/environment';
+import * as fromSearchBox from '@app/modules/xxx-search-box/xxx-search-box.reducer';
 
 export interface State {
+  router: fromRouter.RouterReducerState;
   searchBox: fromSearchBox.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
-  router: routerReducer,
+  router: fromRouter.routerReducer,
   searchBox: fromSearchBox.reducer
 };
 
