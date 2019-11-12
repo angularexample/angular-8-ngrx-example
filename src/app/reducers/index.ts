cc68b8364774a3d41823f2d736bef1a739ea17b0
AngularExample <info@angularexample.com>
2019-11-12 12:14:51 -0500

    Add redux to questions page. Step 2, add new reducer file with ng generate.

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -3,16 +3,19 @@ import * as fromRouter from '@ngrx/router-store';
 
 import { environment } from '@env/environment';
 import * as fromSearchBox from '@app/modules/xxx-search-box/xxx-search-box.reducer';
+import * as fromXxxQuestionsPage from '@app/modules/xxx-questions-page/xxx-questions-page.reducer';
 
 export interface State {
   router: fromRouter.RouterReducerState;
   searchBox: fromSearchBox.State;
+  [fromXxxQuestionsPage.xxxQuestionsPageFeatureKey]: fromXxxQuestionsPage.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
   router: fromRouter.routerReducer,
   searchBox: fromSearchBox.reducer
-};
+
+  [fromXxxQuestionsPage.xxxQuestionsPageFeatureKey]: fromXxxQuestionsPage.reducer,};
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
   ? []
