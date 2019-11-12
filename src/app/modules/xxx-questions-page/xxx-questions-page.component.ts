4472a474f60cf2d24274bc91d2732493204a6eac
AngularExample <info@angularexample.com>
2019-11-12 10:42:42 -0500

    Questions paging works.

--- src/app/modules/xxx-questions-page/xxx-questions-page.component.ts
+++ src/app/modules/xxx-questions-page/xxx-questions-page.component.ts
@@ -1,9 +1,16 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { environment } from '@env/environment';
-import { XxxAlertService, XxxAlertType, XxxDataService} from '@app/xxx-common';
+import { XxxAlertService, XxxAlertType, XxxDataService } from '@app/xxx-common';
 
 @Component({
   selector: 'xxx-questions-page',
@@ -11,7 +18,6 @@ import { XxxAlertService, XxxAlertType, XxxDataService} from '@app/xxx-common';
   templateUrl: './xxx-questions-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class XxxQuestionsPageComponent implements OnDestroy, OnInit {
   pageNumber: number = null;
   isMorePages = false;
@@ -24,12 +30,12 @@ export class XxxQuestionsPageComponent implements OnDestroy, OnInit {
   private subscriptionRouteParam: Subscription;
 
   constructor(
-      private route: ActivatedRoute,
-      private changeDetectorRef: ChangeDetectorRef,
-      private xxxAlertService: XxxAlertService,
-      private xxxDataService: XxxDataService
-  ) {
-  }
+    private route: ActivatedRoute,
+    private router: Router,
+    private changeDetectorRef: ChangeDetectorRef,
+    private xxxAlertService: XxxAlertService,
+    private xxxDataService: XxxDataService
+  ) {}
 
   ngOnInit(): void {
     this.subscribeToRouteParams();
@@ -40,7 +46,7 @@ export class XxxQuestionsPageComponent implements OnDestroy, OnInit {
   }
 
   decodeHtmlEntities(text) {
-    if ((text === undefined) || (text === '')) {
+    if (text === undefined || text === '') {
       return '';
     }
     const doc = new DOMParser().parseFromString(text, 'text/html');
@@ -50,48 +56,48 @@ export class XxxQuestionsPageComponent implements OnDestroy, OnInit {
   }
 
   goToFirstPage() {
-    // const eventRoute: XxxEventRoute = {
-    //   url: [environment.url.questions],
-    //   queryParams: {
-    //     title: this.searchText
-    //   }
-    // };
+    const url = [environment.url.questions];
+    const queryParams = {
+      title: this.searchText
+    };
+    this.router.navigate(url, { queryParams: queryParams });
   }
 
   goToPreviousPage() {
-    // const eventRoute: XxxEventRoute = {
-    //   url: [environment.url.questions],
-    //   queryParams: {
-    //     title: this.searchText,
-    //     page: (this.pageNumber > 2) ? this.pageNumber - 1 : null
-    //   }
-    // };
+    const url = [environment.url.questions];
+    const page = this.pageNumber > 2 ? this.pageNumber - 1 : null;
+    const queryParams = {
+      title: this.searchText,
+      page: page
+    };
+    this.router.navigate(url, { queryParams: queryParams });
   }
 
   goToNextPage() {
-    // const eventRoute: XxxEventRoute = {
-    //   url: [environment.url.questions],
-    //   queryParams: {
-    //     title: this.searchText,
-    //     page: this.pageNumber + 1
-    //   }
-    // };
+    const url = [environment.url.questions];
+    const queryParams = {
+      title: this.searchText,
+      page: this.pageNumber + 1
+    };
+    this.router.navigate(url, { queryParams: queryParams });
   }
 
   questionOnClick(questionId) {
-    // const eventRoute: XxxEventRoute = {
-    //   url: [environment.url.answers + '/' + questionId]
-    // };
+    const url = [environment.url.answers + '/' + questionId];
+    this.router.navigate(url);
   }
 
   private subscribeToRouteParams() {
     this.subscriptionRouteParam = this.route.queryParams.subscribe(params => {
       const searchText = params['title'] || '';
       const pageNumber = +params['page'] || 1;
-      if ((typeof searchText === 'string') && (searchText.length > 0)) {
+      if (typeof searchText === 'string' && searchText.length > 0) {
         this.processSearchQuery(searchText, pageNumber);
       } else {
-        this.xxxAlertService.openAlert(XxxAlertType.WARN, 'Title missing. Try a new search.');
+        this.xxxAlertService.openAlert(
+          XxxAlertType.WARN,
+          'Title missing. Try a new search.'
+        );
       }
     });
   }
@@ -125,25 +131,29 @@ export class XxxQuestionsPageComponent implements OnDestroy, OnInit {
     url += '&page=' + this.pageNumber.toString();
     url += '&order=desc';
     url += '&sort=votes';
-    this.xxxDataService.getData(url)
-        .subscribe(result => this.onSuccessGetQuestions(result),
-            () => this.onErrorGetQuestions());
+    this.xxxDataService.getData(url).subscribe(
+      result => this.onSuccessGetQuestions(result),
+      () => this.onErrorGetQuestions()
+    );
   }
 
   private onSuccessGetQuestions(result) {
     this.isBusy = false;
-    if ((result.hasOwnProperty('items'))
-        && (typeof result.items === 'object')
-        && (result.items.length > 0)) {
+    if (
+      result.hasOwnProperty('items') &&
+      typeof result.items === 'object' &&
+      result.items.length > 0
+    ) {
       this.questions = result.items;
       this.isResult = true;
     } else {
       const warningMsg = 'No Results Found';
       this.xxxAlertService.openAlert(XxxAlertType.WARN, warningMsg);
     }
-    this.isMorePages = ((result.hasOwnProperty('has_more'))
-        && (typeof result.has_more === 'boolean')
-        && (result.has_more));
+    this.isMorePages =
+      result.hasOwnProperty('has_more') &&
+      typeof result.has_more === 'boolean' &&
+      result.has_more;
     this.searchDone();
     this.changeDetectorRef.detectChanges();
   }
@@ -156,6 +166,5 @@ export class XxxQuestionsPageComponent implements OnDestroy, OnInit {
     this.searchDone();
   }
 
-  private searchDone() {
-  }
+  private searchDone() {}
 }
